5cf42e656320eb148773cb7ad9cbe49c4c2ad589
AlexSM8520 <alexandematute@gmail.com>
2025-11-29 18:56:16 -0600

    update token validationw

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,10 @@
 var express = require('express');
 var router = express.Router();
-const validateApiKey = require('../middleware/apiKey');
 const {validateSupabaseToken, optionalSupabaseToken} = require('../middleware/supabaseAuth');
 
 var usersRouter = require('./users');
 var uploadVideoRouter = require('./uploadVideo');
 
-// Apply API key validation to all routes
-// router.use(validateApiKey);
 
 router.use(optionalSupabaseToken);
 // Mount API routes
